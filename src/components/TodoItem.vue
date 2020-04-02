51d5cbb8ad99e93605fc8a0759682dc362b34705
Drew Muliou <Andreyjedai@tut.by>
2020-04-02 11:32:58 +0300

    Finish simple todo app on Vue

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,7 +3,7 @@
 		<span v-bind:class="{ done: todo.completed }">
 			<input type="checkbox" v-on:change="todo.completed = !todo.completed" />
 			<strong>{{ index + 1 }}</strong>
-			{{ todo.title }}
+			{{ todo.title | uppercase }}
 		</span>
 		<button class="rm" v-on:click="$emit('remove-todo', todo.id)">
 			&times;
@@ -20,6 +20,11 @@ export default {
 		},
 		index: {
 			type: Number,
+		},
+	},
+	filters: {
+		uppercase(value) {
+			return value.toUpperCase();
 		},
 	},
 };
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h2>Home Page</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati totam
+			eius aperiam quasi quisquam dolor minima nihil corrupti maiores, ipsam
+			quam atque eum rerum quibusdam laboriosam magnam dicta, molestiae velit.
+		</p>
+		<router-link to="/todos">Todos</router-link>
+	</div>
+</template>
